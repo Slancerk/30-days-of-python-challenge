6dff8c4eb3d8040cdd3eb976447f2982de0d04d6
Slancerk <psouvik260@gmail.com>
2023-10-26 05:40:09 +0530

    findavg.py added

---
+++ array/findavg.py
@@ -0,0 +1,22 @@
+#find average in an array
+
+from array import *
+
+arr=array('i',[])
+
+num = int(input('enter the array size'))
+
+for i in range(num):
+    x=int(input('enter the element'))
+    arr.append(x)
+
+print(arr)
+
+sum=0
+for i in range(num):
+    sum=sum+arr[i]
+
+
+print(sum)
+a=sum/num
+print(f'average is ',a)
